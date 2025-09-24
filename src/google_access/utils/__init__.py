778dcc0406d3b607c030b8c2a1d579ab145fef51
elikent <elikent@gmail.com>
2025-09-24 10:38:16 -0400

    feat: complete log_utils and create projects"

--- src/google_access/utils/__init__.py
+++ src/google_access/utils/__init__.py
@@ -5,9 +5,12 @@ Shared utilities and constants for Google API access.
 
 from .constants import SCOPES, MIME_TYPES  # from constants.py in this folder, import SCOPES
 from .text_utils import remove_diacritics, canon
+from .log_utils import logging_config
+
 
 __all__ = ["SCOPES", 
            "MIME_TYPES", 
            "remove_diacritics", 
-           "canon"
+           "canon",
+           "logging_config"
            ]  # add to public interface
